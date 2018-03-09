883f028d06a9a8fbaf5884b0b5c317f5120b4054
Lakshmi Narasimhan <lakshmi@lakshminp.com>
2018-03-09 17:10:41 +0530

    Before parallelizing yield.

--- parse_pipeline.py
+++ parse_pipeline.py
@@ -1,4 +1,4 @@
-from os import path
+import os
 import csv
 import glob
 import logging
@@ -9,39 +9,50 @@ class ParsePipeline(object):
     
     def __init__(self, data_dir):
         self._data_dir = data_dir
-        self.patient_data = self.get_patient_data(path.join(self._data_dir, 'link.csv'))
+        self.patient_data = self.get_patient_data(os.path.join(self._data_dir, 'link.csv'))
 
     def get_patient_data(self, filename):
         patient_data = []
         with open(filename) as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
+                #TODO: throw exception if patient_id or original_id directory does not exist?
                 patient_data.append((row['patient_id'], row['original_id']))
         return patient_data
 
     def pair_dicom_contour_file(self):
         for patient_id, original_id in self.patient_data:
-            icontour_glob = path.join(self._data_dir, 'contourfiles', original_id, 'i-contours/*.txt')
+            contour_dir = os.path.join(self._data_dir, 'contourfiles', original_id)
+            dicom_dir = os.path.join(self._data_dir, 'dicoms', patient_id)
+            icontour_glob = os.path.join(contour_dir, 'i-contours/*.txt')
             # Use glob iteratively if there are lots of files
             icontour_paths = glob.iglob(icontour_glob)
             for icontour_file in icontour_paths:
-                parts = path.basename(icontour_file).split('-')
-                dicom_num = parts[2].lstrip('0')
-                dicom_file = path.join(self._data_dir, 'dicoms', patient_id, dicom_num + '.dcm')
-                if path.isfile(dicom_file):
-                    yield (dicom_file, icontour_file)
+                dicom_file = self.find_dicom_file_for_icontour_file(dicom_dir, icontour_file)
+                if dicom_file:
+                    yield self.convert_tuple_to_img_boolean_mask(dicom_file, icontour_file)
+
+    def find_dicom_file_for_icontour_file(self, dicom_dir, icontour_file):
+        parts = os.path.basename(icontour_file).split('-')
+        try:
+            dicom_num = parts[2].lstrip('0')
+        except IndexError:
+            return None
+        dicom_file = os.path.join(dicom_dir, dicom_num + '.dcm')
+        if os.path.isfile(dicom_file):
+            return dicom_file
+        #TODO: probably write a debug log saying no dicomfile found for icontour_file
+        return None
 
     def convert_tuple_to_img_boolean_mask(self, dicom_file, icontour_file):
         dcm_dict = parsing.parse_dicom_file(dicom_file)
         if dcm_dict is None:
             logging.warning('Dicom file invalid: ' + dicom_file)
-            return None, None, None
+            return (None, None)
         dicom_img = dcm_dict['pixel_data']
-
-        coords_lst = parsing.parse_contour_file(cipath)
+        coords_lst = parsing.parse_contour_file(icontour_file)
         if len(coords_lst) == 0:
-            logging.warning('Inner contour file empty: ' + cipath)
-            return None, None, None
+            logging.warning('Inner contour file empty: ' + icontour_file)
+            return (None, None)
         icontour_boolean_mask = parsing.poly_to_mask(coords_lst, dicom_img.shape[0], dicom_img.shape[1])
         return (dicom_img, icontour_boolean_mask)
-        
--- test_parse_pipeline.py
+++ test_parse_pipeline.py
@@ -5,18 +5,59 @@ from parse_pipeline import ParsePipeline
 
 class TestParsePipeline(unittest.TestCase):
 
+    def setUp(self):
+        # TODO: change to test_data
+        self.pl1 = ParsePipeline('./final_data')
+        self.valid_dicom_file = './test_data/dicoms/study-1/48.dcm'
+        self.invalid_dicom_file = './test_data/dicoms/study-1/invalid.dcm'
+        self.valid_icontour_file = './test_data/contourfiles/cf-1/i-contours/IM-0001-0048-icontour-manual.txt'
+        self.invalid_icontour_file = './test_data/contourfiles/cf-1/i-contours/invalid-icontour.txt'
+        
     def test_patient_data(self):
-        pl1 = ParsePipeline('./final_data')
-        print pl1.patient_data
-        self.assertTrue(len(pl1.patient_data) == 5)
+        self.assertTrue(len(self.pl1.patient_data) == 5)
+
+    def test_find_dicom_file_for_icontour_file_invalid_icontour_file_hyphen(self):
+        pl = ParsePipeline('./test_data')
+        ret = pl.find_dicom_file_for_icontour_file('./test_data/dicoms/study-2', './test_data/contourfiles/cf-2/i-contours/invalid-icontour.txt')
+        self.assertIsNone(ret)
+
+    def test_find_dicom_file_for_icontour_file_invalid_icontour_file_nohyphen(self):
+        pl = ParsePipeline('./test_data')
+        ret = pl.find_dicom_file_for_icontour_file('./test_data/dicoms/study-2', './test_data/contourfiles/cf-2/i-contours/invalidicontour.txt')
+        self.assertIsNone(ret)
+
+    def test_find_dicom_file_for_icontour_file_non_existent(self):
+        pl = ParsePipeline('./test_data')
+        ret = pl.find_dicom_file_for_icontour_file('./test_data/dicoms/study-2', './test_data/contourfiles/cf-2/i-contours/IM-0001-0147-icontour-manual.txt.txt')
+        self.assertIsNone(ret)
+
+    def test_find_dicom_file_for_icontour_file(self):
+        pl = ParsePipeline('./test_data')
+        ret = pl.find_dicom_file_for_icontour_file('./test_data/dicoms/study-2', './test_data/contourfiles/cf-2/i-contours/IM-0001-0140-icontour-manual.txt.txt')
+        self.assertEqual(ret, './test_data/dicoms/study-2/140.dcm')
+        ret = pl.find_dicom_file_for_icontour_file('./test_data/dicoms/study-2', './test_data/contourfiles/cf-2/i-contours/IM-0001-0127-icontour-manual.txt.txt')
+        self.assertEqual(ret, './test_data/dicoms/study-2/127.dcm')
 
     def test_pair_dicom_contour_file(self):
-        pl1 = ParsePipeline('./final_data')
-        pair = pl1.pair_dicom_contour_file()
+        #TODO: add corresponding test data
+        pair = self.pl1.pair_dicom_contour_file()
         for i in range(10):
-            dicom, icont = next(pair)
-            self.assertTrue(path.isfile(dicom))
-            self.assertTrue(path.isfile(icont))
+            dicom_image, boolean_mask = next(pair)
+            self.assertEqual(boolean_mask.shape, dicom_image.shape)
+
+    def test_convert_tuple_to_img_boolean_mask_invalid_dicom(self):
+        (dicom_img, boolean_mask) = self.pl1.convert_tuple_to_img_boolean_mask(self.invalid_dicom_file, self.valid_icontour_file)
+        self.assertIsNone(dicom_img)
+        self.assertIsNone(boolean_mask)
+
+    def test_convert_tuple_to_img_boolean_mask_invalid_icontour(self):
+        (dicom_img, boolean_mask) = self.pl1.convert_tuple_to_img_boolean_mask(self.valid_dicom_file, self.invalid_icontour_file)
+        self.assertIsNone(dicom_img)
+        self.assertIsNone(boolean_mask)
+
+    def test_convert_tuple_to_img_boolean_mask(self):
+        (dicom_img, boolean_mask) = self.pl1.convert_tuple_to_img_boolean_mask(self.valid_dicom_file, self.valid_icontour_file)
+        self.assertEqual(dicom_img.shape, boolean_mask.shape)
 
 if __name__ == '__main__':
     unittest.main()
